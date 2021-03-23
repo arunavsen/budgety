e005eb759d968857c42bdae137e6d94d8039741e
Arunav <arunav@vonome.com>
2021-03-23 20:52:26 +0600

    Setting up First EventListeners

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
+// BUDJET Controller
 var budgetController = (function(){
-    var x = 23;
-    var add = function(a){
-        return x+a;
-    }
-
-    var minus = function(b){
-        return x-b;
-    }
-
-    return{
-        publicTest: function(b){
-            return add(b);
-        },
-        publicTest2: function(x){
-            console.log(minus(x));
-        }
-    }
+    
 })();
 
 
+
+// UI Controller
 var UIController = (function(){
     // Some code
 })();
 
 
-var controller = (function(budjetCtrl,UICtrl){
-    var o = budjetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function(){
-            console.log(o);
-        }
+// Global App Controller
+var controller = (function(budjetCtrl,UICtrl){
+    
+    var ctrlAddItem = function(){
+         // 1. Get the field input data
+
+        // 2. Add the item to the BudjetController
+
+        // 3. Add Item to the UI
+
+        // 4. Calculate the budjet
+
+        // 5. Display the budjet on the UI
+        console.log("I am not robot");
     }
+
+
+
+
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+
+
+    // If we press the Enter button for adding the product
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode==13 || event.which==13){
+            ctrlAddItem();
+        }
+    });
 })(budgetController,UIController);
