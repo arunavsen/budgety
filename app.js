e65f0f061b6d044a3b53add92d68faf17ea932cd
Arunav <arunav@vonome.com>
2021-03-24 12:49:35 +0600

    Creating an Initialization function

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // BUDJET Controller
-var budgetController = (function(){
-    
+var budgetController = (function () {
+
 })();
 
 
 
 // UI Controller
-var UIController = (function(){
-    
+var UIController = (function () {
+
     // We are creating a private object 'DOMstrings' by which we can change our class dynamicaaly
     var DOMstrings = {
         inpuType: '.add__type',
-        inputDescription:'.add__description',
-        inputValue:'.add__value',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
         inputAddBtn: '.add__btn'
     }
-    
+
     return {
         //  This controller is returning the Inputs by getInput function
-        getInput:function(){
+        getInput: function () {
             return {
                 type: document.querySelector(DOMstrings.inpuType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
@@ -26,7 +26,7 @@ var UIController = (function(){
             };
         },
         // our private DOMstrings now will be public
-        getDomstrings:function(){
+        getDomstrings: function () {
             return DOMstrings;
         }
     };
@@ -35,16 +35,28 @@ var UIController = (function(){
 
 
 // Global App Controller
-var controller = (function(budjetCtrl,UICtrl){
+var controller = (function (budjetCtrl, UICtrl) {
 
-    // We can get all DOMstrings in the 'controller' from UIController. It is now public
-    var DOM = UICtrl.getDomstrings();
+    var setupEventListeners = function () {
+        // We can get all DOMstrings in the 'controller' from UIController. It is now public
+        var DOM = UICtrl.getDomstrings();
 
-    var ctrlAddItem = function(){
+        // If we click the add_btn
+        document.querySelector(DOM.inputAddBtn).addEventListener('click', ctrlAddItem);
+
+        // If we press the Enter button for adding the product
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode == 13 || event.which == 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+
+    var ctrlAddItem = function () {
         // 1. Get the field input data
-
         var input = UICtrl.getInput();
-        console.log(input);
+        
         // 2. Add the item to the BudjetController
 
         // 3. Add Item to the UI
@@ -54,13 +66,13 @@ var controller = (function(budjetCtrl,UICtrl){
         // 5. Display the budjet on the UI
     }
 
-    // If we click the add_btn
-    document.querySelector(DOM.inputAddBtn).addEventListener('click',ctrlAddItem);
-
-    // If we press the Enter button for adding the product
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode==13 || event.which==13){
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log("Application has started");
+            setupEventListeners();
         }
-    });
-})(budgetController,UIController);
+    }
+
+})(budgetController, UIController);
+
+controller.init();
