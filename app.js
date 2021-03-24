18e9c46beb4f6391a6749a7f87a8bae16bc64bf5
Arunav <arunav@vonome.com>
2021-03-24 21:07:29 +0600

    Adding a New Item to the UI

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 // BUDJET Controller
 var budgetController = (function () {
-    var Expense = function(id, description, value){
+    var Expense = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
-    var Income = function(id, description, value){
+    var Income = function (id, description, value) {
         this.id = id;
         this.description = description;
         this.value = value;
     };
 
     var data = {
-        allItems:{
-            exp:[],
-            inc:[]
+        allItems: {
+            exp: [],
+            inc: []
         },
-        totals:{
-            exp:0,
-            inc:0
+        totals: {
+            exp: 0,
+            inc: 0
         }
     };
 
-    return{
-        addItem: function(type, des, val){
+    return {
+        addItem: function (type, des, val) {
             var newItem;
 
             // Create new Id
-            if(data.allItems[type].length>0){
-                id = data.allItems[type][data.allItems[type].length-1].id+1;
-            }else{
-                id=0;
+            if (data.allItems[type].length > 0) {
+                id = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                id = 0;
             }
 
             // Create new item based on 'inc' or 'exp'
-            if(type === 'exp'){
+            if (type === 'exp') {
                 newItem = new Expense(id, des, val);
-            }else if(type === 'inc'){
+            } else if (type === 'inc') {
                 newItem = new Income(id, des, val);
             }
 
@@ -47,7 +47,7 @@ var budgetController = (function () {
             // Return the new element
             return newItem;
         },
-        testing: function(){
+        testing: function () {
             console.log(data);
         }
     };
@@ -62,7 +62,9 @@ var UIController = (function () {
         inpuType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputAddBtn: '.add__btn'
+        inputAddBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expressContainer: '.expenses__list'
     }
 
     return {
@@ -74,6 +76,29 @@ var UIController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+
+        addListItem: function (obj, type) {
+            var html, newHtml, element;
+
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expressContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%',obj.id);
+            newHtml = newHtml.replace('%description%',obj.description);
+            newHtml = newHtml.replace('%value%',obj.value);
+
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+        },
+
         // our private DOMstrings now will be public
         getDomstrings: function () {
             return DOMstrings;
@@ -112,6 +137,7 @@ var controller = (function (budjetCtrl, UICtrl) {
         newItem = budjetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add Item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budjet
 
@@ -119,7 +145,7 @@ var controller = (function (budjetCtrl, UICtrl) {
     }
 
     return {
-        init: function() {
+        init: function () {
             console.log("Application has started");
             setupEventListeners();
         }
