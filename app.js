c167261509a1cf0a4375678acf2c1b6a55a59031
Arunav <arunav@vonome.com>
2021-03-31 13:07:18 +0600

    Format our Budget Number string manipulation

--- app.js
+++ app.js
@@ -158,6 +158,30 @@ var UIController = (function () {
     expensePercLabel:'.item__percentage'
   };
 
+  var formatNumber = function(num, type){
+    var numSplit, int, dec, type;
+    /*
+      i) + or - before number
+     ii) exctly 2 decimal points
+    iii) comma separating the thousands
+     ex. 2310.4576 -> +2,310.46
+         2000 -> 2,000.00
+         25130 -> + 25,130.00
+    */
+
+    num = Math.abs(num);
+    num = num.toFixed(2);
+    numSplit = num.split('.');
+
+    int = numSplit[0];
+    dec = numSplit[1];
+    if(int.length>3){
+      int = int.substr(0,int.length - 3)+','+int.substr(int.length-3, int.length);
+    }
+
+    return (type === 'exp'?'-':'+')+' '+ int+'.'+dec;
+  };
+
   return {
     //  This controller is returning the Inputs by getInput function
     getInput: function () {
@@ -185,7 +209,7 @@ var UIController = (function () {
       // Replace the placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id);
       newHtml = newHtml.replace("%description%", obj.description);
-      newHtml = newHtml.replace("%value%", obj.value);
+      newHtml = newHtml.replace("%value%", formatNumber(obj.value, type));
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
@@ -213,9 +237,12 @@ var UIController = (function () {
     },
 
     displayBudget:function(obj){
-      document.querySelector(DOMstrings.budgetLevel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+      var type;
+      obj.budget > 0 ? type = 'inc': type='exp';
+
+      document.querySelector(DOMstrings.budgetLevel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+      document.querySelector(DOMstrings.expenseLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
       if(obj.percentage > 0){
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage+'%';
